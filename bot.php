5ed4842ed2f02b48124cac1b43fb305e93b4b163
swunews <swusci.news@gmail.com>
2017-07-05 12:56:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -31,7 +31,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "sticker";
   $arrPostData['messages'][0]['packageId'] = "1";
-  $arrPostData['messages'][0]['stickerId'] = "5";
+  $arrPostData['messages'][0]['stickerId'] = "5",
   $arrPostData['messages'][0]['stickerId'] = "3";
 }
 
