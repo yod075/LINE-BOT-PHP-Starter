75a70a82815270ab3ad7b1afc846c46fe332411c
swunews <swusci.news@gmail.com>
2017-07-05 17:34:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -37,7 +37,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "template";
-  $arrPostData['button'][0]['text'] = "OK";
+  $arrPostBackData['button'][0]['text'] = "OK";
   //$arrPostData['template'][0]['action'] = "OK";
 
 }
