3748bbadb7281a128a76623ea8103ab0afa35e13
swunews <swusci.news@gmail.com>
2017-07-07 15:27:40 +0700

    Update connetion.php

--- connetion.php
+++ connetion.php
@@ -1,5 +1,5 @@
 <?php
-$con= mysqli_connect("localhost","root","123","text") or die("Error: " . mysqli_error($con));
+$con= mysqli_connect("localhost","root","123","chatbot") or die("Error: " . mysqli_error($con));
  
 mysqli_query($con, "SET NAMES 'utf8' ");
  
