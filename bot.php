0beb31db17b06efa866730a245636c29db4186c7
swunews <swusci.news@gmail.com>
2017-07-07 16:19:53 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -40,17 +40,25 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['type'] = "sticker";
   $arrPostData['messages'][0]['packageId'] = "1";
   
-  $stickerRand = rand(0,5);
-  if($textRand == 0){
+  $stickerRand = rand(0,7);
+  if($stickerRand  == 0){
 	  $arrPostData['messages'][0]['stickerId'] = "3";
-  }else if($textRand == 1){
+  }else if($stickerRand  == 1){
 	  $arrPostData['messages'][0]['stickerId'] = "11";
-  }else if($textRand == 2){
+  }else if($stickerRand  == 2){
 	 $arrPostData['messages'][0]['stickerId'] = "2";
-  }else if($textRand == 3){
+  }else if($stickerRand  == 3){
 	 $arrPostData['messages'][0]['stickerId'] = "9";
-  }else {
+  }else if($stickerRand  == 4){
 	  $arrPostData['messages'][0]['stickerId'] = "7";
+  }else if($stickerRand  == 5){
+	 $arrPostData['messages'][0]['stickerId'] = "9";
+  }else if($stickerRand  == 6){
+	 $arrPostData['messages'][0]['stickerId'] = "20";
+  }else if($stickerRand  == 7){
+	 $arrPostData['messages'][0]['stickerId'] = "25";
+  }else{
+	 $arrPostData['messages'][0]['stickerId'] = "22";
   }
 
 
