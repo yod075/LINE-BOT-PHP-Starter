9206a99415679f06d888f63fe6dfb54ab4ff8401
swunews <swusci.news@gmail.com>
2017-07-07 17:38:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -35,11 +35,11 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['type'] = "image";
   $arrPostData['messages'][0]['originalContentUrl'] = 'https://github.com/swunews/LINE-BOT-PHP-Starter/blob/master/grief-and-loss.jpg?raw=true';
   $arrPostData['messages'][0]['previewImageUrl'] = 'https://github.com/swunews/LINE-BOT-PHP-Starter/blob/master/grief-and-loss_preview.jpg?raw=true';	
-}else if($arrJson['events'][0]['message']['text'] == "เข้า"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "template";
-  $arrPostData['message'][0]['altText'] = "confrim";
+//}else if($arrJson['events'][0]['message']['text'] == "เข้า"){
+  //$arrPostData = array();
+  //$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  //$arrPostData['messages'][0]['type'] = "template";
+  //$arrPostData['message'][0]['altText'] = "confrim";
   //$arrPostData['template'][0]['button'] = "login";
   //$arrPostData['button'][0]['actions'] = 
   //$arrPostData['message'][0]['type'] = "text",$arrPostData['message'][0]['label'] = "ok",$arrPostData['message'][0]['text'] = "ok",;
