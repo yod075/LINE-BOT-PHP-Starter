a18f2ef88c2151d27f8c4eed8bfd6834e0d3092a
swunews <swusci.news@gmail.com>
2017-07-18 14:45:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -50,7 +50,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"||$arrJson['e
 }else if($arrJson['events'][0]['message']['text'] == "เข้าสู่ระบบ"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['template'] = "confirm";
+  $arrPostData['template'][0]['type'] = "confirm";
 
  
 }else{
