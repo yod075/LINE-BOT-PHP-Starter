fec08bcfe53c9f9f9c1d9bebc9ea3cba5c3151f8
swunews <swusci.news@gmail.com>
2017-07-19 15:34:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -40,7 +40,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"||$arrJson['e
   $arrPostData['messages'][0]['type'] = "location";
   $arrPostData['messages'][0]['title'] = "มหาวิทยาลัยศรีนครินทรวิโรฒ";
   $arrPostData['messages'][0]['address'] = "สุขุมวิท 23, แขวงคลองเตยเหนือ เขตวัฒนา กรุงเทพ, 10110";
-  $arrPostData['messages'][0]['latitude'] = "13.745430";
+ // $arrPostData['messages'][0]['latitude'] = "13.745430";
   $arrPostData['messages'][0]['longitude'] = "100.565264";
 }else if($arrJson['events'][0]['message']['text'] == "เข้าสู่ระบบ"){
  $arrPostData = array();
