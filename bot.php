fe39285a67ef54869341331d36dd46f791fa4c4e
swunews <swusci.news@gmail.com>
2017-07-19 15:37:21 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -42,10 +42,11 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"||$arrJson['e
   $arrPostData['messages'][0]['address'] = "สุขุมวิท 23, แขวงคลองเตยเหนือ เขตวัฒนา กรุงเทพ, 10110";
   $arrPostData['messages'][0]['latitude'] = "13.745430";
   $arrPostData['messages'][0]['longitude'] = "100.565264";
-}else if($arrJson['events'][0]['message']['text'] == "เข้าสู่ระบบ"){
+}else if($arrJson['events'][0]['message']['text'] == "login"){
  $arrPostData = array();
  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
- $arrPostData['confirm'][0]['type'] = "confirm"; 
+ $arrPostData['messages'][0]['type'] = "confirm"; 
+ $arrPostData['messages'][0]['title'] = "มหาวิทยาลัยศรีนครินทรวิโรฒ";
  
 }else{
   $arrPostData = array();
